157b22f4b01cfe2a56aec6a1fbc78a419da82dab
Peter Nagy <peternagyxx@gmail.com>
2014-08-21 09:25:00 +0200

    Tiny changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var spawn = require('child_process').spawn;
 var sourcemaps = require('gulp-sourcemaps');
+var wait = require('gulp-wait')
 var browserSync = require('browser-sync');
 var node;
 
@@ -9,31 +10,41 @@ var node;
  * $ gulp server
  * description: launch the server. If there's a server already running, kill it.
  */
-// gulp.task('server', function() {
+// gulp.task('express-server', function() {
 //   if (node) node.kill()
-//   node = spawn('node', ['out/csvDataSource.js'], {stdio: 'inherit'})
+//   node = spawn('node', ['./bin/www'], {stdio: 'inherit'})
 //   node.on('close', function (code) {
 //     if (code === 8) {
 //       console.log('Error detected, waiting for changes...');
-//       gulp.start('server');
+//       gulp.start('express-server');
 //     }
 //   });
 //   node.on('error', function () {
 //     console.log('Error detected, waiting for changes...');
-//     gulp.start('server');
+//     gulp.start('express-server');
 //   });
+//   wait(1500);
 // })
 
-gulp.task('coffee', function() {
-    gulp.src('./public/javascripts/*.coffee')
+gulp.task('client-side-coffee', function() {
+    var scriptDir = './public/javascripts/';
+    gulp.src(scriptDir + '*.coffee')
         .pipe(sourcemaps.init())
         .pipe(coffee({bare: true}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./public/javascripts'))
+        .pipe(sourcemaps.write(scriptDir))
+        .pipe(gulp.dest(scriptDir))
 });
 
+// gulp.task('server-side-coffee', function() {
+//     gulp.src('./public/javascripts/*.coffee')
+//         .pipe(sourcemaps.init())
+//         .pipe(coffee({bare: true}))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest('./public/javascripts'))
+// });
+
 // Reload all Browsers
-gulp.task('bs-reload', function () {
+gulp.task('browser-sync-reload', function () {
     browserSync.reload();
 });
 
@@ -49,10 +60,14 @@ gulp.task('browser-sync', function() {
 
 gulp.task('watch', function()
 {
-    gulp.watch(['./*.coffee'], ['coffee']);
-    gulp.watch(['views/*.jade'], ['bs-reload']);
-    gulp.watch(['public/stylesheets/*.styl'], ['bs-reload']);
-    gulp.watch(['public/javascripts/*.js'], ['bs-reload']);
+    //Client side watch
+    gulp.watch(['views/*.jade'], ['browser-sync-reload']);
+    gulp.watch(['public/stylesheets/*.styl'], ['browser-sync-reload']);
+    gulp.watch(['public/javascripts/*.coffee'], ['client-side-coffee', 'browser-sync-reload']);
+
+    //Server side watch
+    // gulp.watch(['routes/*.js'], ['express-server', 'browser-sync-reload']);
+    // gulp.watch(['*.js'], ['express-server', ,'browser-sync-reload']);
 });
 
-gulp.task('default', ['watch', 'browser-sync']);
+gulp.task('default', ['browser-sync', 'watch']);
